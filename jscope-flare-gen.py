5434c732fa2bdde8100045b1b74b855c65238afe
Sean <sean.alsop@d-tacq.co.uk>
2020-11-10 16:01:32 +0000

    Added two columns to jscope-flare-gen.py 	modified:   jscope-flare-gen.py

--- jscope-flare-gen.py
+++ jscope-flare-gen.py
@@ -23,13 +23,16 @@ def get_default_jscp():
 
 
 def create_new_jscp(args, text):
+    col = 1
     new_text = ""
     text += "Scope.plot_1_1.x_expr_1: \TOP:TRANSIENT1:TB_NS\n"
     text += "Scope.plot_1_1.y_expr_1: \TOP:TRANSIENT1:INPUT_001:CAL_INPUT\n"
     text += "Scope.plot_1_1.num_shot: 1\n"
 
+    text = text.replace("Scope.rows_in_column_2: 1",
+                        "Scope.rows_in_column_2: {}".format(len(args.uuts)//2))
     text = text.replace("Scope.rows_in_column_1: 1",
-                        "Scope.rows_in_column_1: {}".format(len(args.uuts)))
+                        "Scope.rows_in_column_1: {}".format(len(args.uuts) - len(args.uuts)//2))
     text = text.replace("Scope.plot_1_1.num_expr: 0",
                         "Scope.plot_1_1.num_expr: 1")
     text = text.replace("Scope.plot_1_1.global_defaults: -1",
@@ -37,22 +40,27 @@ def create_new_jscp(args, text):
 
     for line in text.split("\n"):
         if line.startswith("Scope.plot_1_1"):
-            line_to_add = line.replace("1_1", "{index}_1")
+            line_to_add = line.replace("1_1", "{index}_{column}")
             new_text += line_to_add + "\n"
 
+    row = 0
     for num, uut in enumerate(args.uuts):
-        if num != 0:
-            text += new_text.format(index=num+1)
-        text += "Scope.plot_{}_1.title: '{} shot: '//$SHOT\n".format(
-            num+1, uut.upper())
-        text += "Scope.plot_{}_1.experiment: {}\n".format(num+1, uut.upper())
-        #text += "Scope.plot_{}_1.num_expr: {}\n".format(num+1, num+1)
-        text += "Scope.plot_{}_1.mode_1D_1_1: Line\n".format(num+1)
-        text += "Scope.plot_{}_1.mode_2D_1_1: xz(y)\n".format(num+1)
-        text += "Scope.plot_{}_1.color_1_1: 0\n".format(num+1)
-        text += "Scope.plot_{}_1.marker_1_1: 0\n".format(num+1)
-        text += "Scope.plot_{}_1.step_marker_1_1: 1\n\n".format(num+1)
+        if num == 8:
+            col = 2
+            row = 0
 
+        if num != 0:
+            text += new_text.format(index=row+1, column=col)
+        text += "Scope.plot_{}_{}.title: '{} shot: '//$SHOT\n".format(
+            row+1, col, uut.upper())
+        text += "Scope.plot_{}_{}.experiment: {}\n".format(row+1, col, uut.upper())
+        #text += "Scope.plot_{}_1.num_expr: {}\n".format(row+1, num+1)
+        text += "Scope.plot_{}_{}.mode_1D_1_1: Line\n".format(row+1, col)
+        text += "Scope.plot_{}_{}.mode_2D_1_1: xz(y)\n".format(row+1, col)
+        text += "Scope.plot_{}_{}.color_1_1: 0\n".format(row+1, col)
+        text += "Scope.plot_{}_{}.marker_1_1: 0\n".format(row+1, col)
+        text += "Scope.plot_{}_{}.step_marker_1_1: 1\n\n".format(row+1, col)
+        row += 1
     print(text)
     return None
 
